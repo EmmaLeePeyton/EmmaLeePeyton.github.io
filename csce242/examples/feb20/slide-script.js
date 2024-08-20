3b275be78e59cda1628ad0d6c0f6b7dcb65f931a
EmmaLee Peyton <epeyton@email.sc.edu>
2024-08-20 12:16:48 -0400

    edits

--- csce242/examples/feb20/slide-script.js
+++
@@ -1,5 +0,0 @@
-const slide = () => {
-    console.log("Sliding");
-};
-
-setInterval(slide,1000);
--- csce242/projects/part4/home/script.js
+++
@@ -1,7 +0,0 @@
-const toggleNav = () => {
-    documant.getElementById("main-nav-items").classList.toggle("hidden");
-};
-
-window.onload = () => {
-    document.getElementById("nav-toggle").onclick = toggleNav;
-};
