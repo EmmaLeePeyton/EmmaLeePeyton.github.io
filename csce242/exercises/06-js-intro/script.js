b974fe849dba2af943203c29f133c3f733a7b76e
EmmaLee Peyton <epeyton@email.sc.edu>
2024-09-11 06:53:45 -0400

    added a little project part 3

---
+++ csce242/exercises/06-js-intro/script.js
@@ -0,0 +1,36 @@
+// alert("Hello World"); // alert after the page is loaded
+
+/*
+//scoping, no use var, can change
+let 
+//every variable is default unless it changes
+const
+*/
+
+/* functions */
+/*function doStuff() { //example to show how it works
+    alert("hello");
+}*/
+// => : arrow syntax
+const doStuff = () =>{
+    alert("Hello world");
+};
+
+// Button
+const myButton = document.getElementById("btn-say-hello");
+//myButton.innerHTML = "Click Party"; //funsies to show change
+//myButton.onclick = doStuff;
+
+myButton.onclick = () => {
+    document.getElementById("message").innerHTML = "Hello Everyone!";
+    document.getElementById("stuff").classList.add("special");
+}; //allows there to be little to no memory used
+
+document.getElementById("btn-say-goodbye") = () => {
+    document.getElementById("message").innerHTML = "goodbye Everyone!";
+    document.getElementById("stuff").classList.remove("special");
+}; 
+
+/*add second button, called say goodbye 
+when clicked change text to goodbye everyone, and remove special style
+*/
