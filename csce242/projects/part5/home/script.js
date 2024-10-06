6c2cf1d96b66aca95f02487d1d0ac403e3212a57
EmmaLee Peyton <epeyton@email.sc.edu>
2024-10-06 02:20:32 -0400

    home, contactus, and chapters done

--- csce242/projects/part5/home/script.js
+++
@@ -1,5 +0,0 @@
-// toggle nav
-document.querySelector("#toggle-nav").onclick = () => {
-    console.log("You toggled the nav!");
-    document.getElementById("nav-items").classList.toggle("hidden-small");
-};
