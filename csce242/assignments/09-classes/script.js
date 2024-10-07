09a30b28c78c57cee017325d9555123c1844dfb9
EmmaLee Peyton <epeyton@email.sc.edu>
2024-10-06 23:19:12 -0400

    I give up

--- csce242/assignments/09-classes/script.js
+++ csce242/assignments/09-classes/script.js
@@ -14,27 +14,27 @@ class Bird {
     const section = document.createElement("section");
     section.classList.add("bird");
 
-    // create link
-    const a = document.createElement("a");
-    a.href = "#";
-    section.append(a)
-
     //create header
     const h3 = document.createElement("h3");
     h3.innerHTML = this.title;
-    a.append(h3);
+    section.append(h3);
 
     //create image and add to first column
-    a.append(this.picture(this.pic));
+    section.append(this.picture(this.pic));
 
+    // Add click event listener
+    section.addEventListener("click", () => this.showDetails());
+
+    
     return section;
   }
 
 
   picture(info){
-    const pic = document.createElement("img");
-    pic.src = "images/" + info + ".jpg";
-    return pic;
+    const img = document.createElement("img");
+    img.src = "images/" + info + ".jpg";
+
+    return img;
   }
 
   showDetails() {
@@ -43,22 +43,34 @@ class Bird {
     const closeBtn = document.querySelector(".close-btn");   
 
 
-    // Set modal content
+    modal.classList.remove("hidden");
     document.getElementById("modal-title").textContent = this.title;
     document.getElementById("modal-size").textContent = this.size;
     document.getElementById("modal-lifespan").textContent = this.lifeSpan;
     document.getElementById("modal-food").textContent = this.food;
     document.getElementById("modal-habitat").textContent = this.habitat;
     document.getElementById("modal-fact").textContent = this.fact;
+    document.getElementById("modal-pic").picture = "images/" + this.pic + ".jpg";
 
-    // Show modal
-    modal.classList.remove("hidden");
-
-    // Close modal on button click
     closeBtn.addEventListener("click", () => {
         modal.classList.add("hidden");
     });
-  } 
+}
+
+}
+
+const birds = [];
+birds.push(new Bird("Summer Cardinal", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "summer-tanager"));
+birds.push(new Bird("Northern Cardinal", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "northern-cardinal"));
+birds.push(new Bird("Pine Grosbeak", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "pine-grosbeak"));
+birds.push(new Bird("Purple Finch", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "purple-finch"));
+
+birds.forEach((bird)=>{
+  document.getElementById("bird-list").append(bird.item);
+});
+  
+
+
 /*
     //make section with dog info then display
     const section = document.createElement("section");
@@ -85,15 +97,3 @@ class Bird {
   
     secondColumn.append(this.picture(this.pic));
 */
-}
-
-const birds = [];
-birds.push(new Bird("Summer Cardinal", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "summer-tanager"));
-birds.push(new Bird("Northern Cardinal", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "northern-cardinal"));
-birds.push(new Bird("Pine Grosbeak", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "pine-grosbeak"));
-birds.push(new Bird("Purple Finch", "29.1 grams", 5, "bees and wasps", "open woodlands, forests, and even our backyards", "before they eat, they skillfully remove the stinger from their prey", "purple-finch"));
-
-birds.forEach((bird)=>{
-  document.getElementById("bird-list").append(bird.item);
-});
-  
